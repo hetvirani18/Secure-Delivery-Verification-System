9cd1b407e7fcbd5bdfa99923bd0dde440677309d
Het Virani <hetvirani87@gmail.com>
2026-07-08 15:14:30 +0530

    make production ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,11 @@ const deliveryRoutes = require('./routes/delivery.route.js');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/clients', clientRoutes);
 app.use('/invoices', invoiceRoutes);
